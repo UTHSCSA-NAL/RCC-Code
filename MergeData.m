511e6422655435735a7db27505e90f1ba6ceaa30
HangfanLiu <43455367+HangfanLiu@users.noreply.github.com>
2023-01-14 15:20:44 -0500

    Delete MergeData.m

--- MergeData.m
+++
@@ -1,4 +0,0 @@
-A = dataset('XLSFile','status.csv');
-C = dataset('XLSFile','merged_pvc.csv');
-joinDiff = join(A,C,'LeftKeys',{'RID'},'RightKeys',{'RID'},'Type','fullouter','MergeKeys',true);
-export(joinDiff,'XLSFile','merged.csv')
